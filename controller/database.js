a0e1f341e2d3c104bad628b8e54a9133b94fa5ce
Rahul Gupta <rahul4gupta1997@gmail.com>
2022-04-01 11:04:09 +0530

    database changes 1.0

--- controller/database.js
+++ controller/database.js
@@ -3,6 +3,9 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('dcgpucpq6uo1hc','hxurszejhsxjsy','e78dc1f82930c4d2baf9c07a38d8549b8146fe79209b4b39dda8f52632cc44f5',{
     host: 'ec2-34-207-12-160.compute-1.amazonaws.com',
     dialect: 'postgres',
+    dialectOptions: {
+        "ssl": true
+      },
     pool: {
         max: 9,
         min:0,
